66f948e88783c593ec375c668c07d8eb7f1cf13b
Jevelin4k <jevelin4k@gmail.com>
2025-04-10 17:43:18 +0200

    Add files via upload

---
+++ uninstall.bat
@@ -0,0 +1,11 @@
+@echo off
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+schtasks /delete /tn "Spotify_ad_blocker" /f
+TASKKILL /F /IM pythonw.exe
+rmdir "C:\Program Files (x86)\Spotify_ad_blocker" /s /q
