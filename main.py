21a422e8b99abfa186ffea47faa244c2f1d5bb8c
Jevelin4k <jevelin4k@gmail.com>
2025-06-16 09:07:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_active_window_title():
 
 def restart_app():
     #тут презапуск спотифай
-    subprocess.Popen(['TASKKILL /F /IM Spotify.exe', "--minimized"], creationflags=subprocess.CREATE_NO_WINDOW)
+    subprocess.Popen('TASKKILL /F /IM Spotify.exe', shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
     spotify_path = os.path.expanduser("~") + "\\AppData\\Local\\Microsoft\\WindowsApps\\Spotify.exe"
 
     subprocess.Popen([spotify_path, "--minimized"], creationflags=subprocess.CREATE_NO_WINDOW)# ////////////////////////////////////
