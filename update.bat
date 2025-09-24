dcb73717041368b0a9e52124509b08cdbc1f0237
Jevelin4k <jevelin4k@gmail.com>
2025-09-24 21:50:55 +0300

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,44 @@
+@echo off
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+schtasks /delete /tn "Spotify_ad_blocker" /f
+
+TASKKILL /F /IM pythonw.exe
+rmdir "C:\Program Files (x86)\Spotify_ad_blocker" /s /q
+
+
+
+winget install python3.11 --accept-source-agreements --silent
+python.exe -m pip install --upgrade pip
+pip install winrt-runtime==3.0.0
+pip install winrt-Windows.Foundation==3.0.0
+pip install winrt-Windows.Foundation.Collections==3.0.0
+pip install winrt-Windows.Media==3.0.0
+pip install winrt-Windows.Media.Audio==3.0.0
+pip install winrt-Windows.Media.Control==3.0.0
+pip install winrt-Windows.Media.Core==3.0.0
+pip install winrt-Windows.Storage==3.0.0
+pip install winrt-Windows.Storage.Streams==3.0.0
+pip install asyncio
+pip install subprocess
+pip install pywin32
+pip install pygetwindow
+pip install sys
+pip install psutil
+
+
+mkdir "C:\Program Files (x86)\Spotify_ad_blocker"
+cd "C:\Program Files (x86)\Spotify_ad_blocker"
+git clone https://github.com/Jevelin4k/Spotify_ad_blocker.git
+
+schtasks /create /tn "Spotify_ad_blocker" /tr "\"C:\Windows\System32\wscript.exe\" \"C:\Program Files (x86)\Spotify_ad_blocker\Spotify_ad_blocker\launch.vbs\"" /sc onlogon /rl highest /f
+
+
+schtasks /run /tn "Spotify_ad_blocker"
+
+pause
